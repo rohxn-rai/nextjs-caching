2387996a3d40accc7e3c0cd1fb5ab9c1ed395cdb
rohxn-rai <rohan.rai932@outlook.com>
2025-02-25 10:28:48 -0500

    Next Cache Settings - added revalidation to the caching setting

--- app/messages/layout.js
+++ app/messages/layout.js
@@ -1,5 +1,9 @@
 export default async function MessagesLayout({ children }) {
-  const response = await fetch("http://localhost:8080/messages");
+  const response = await fetch("http://localhost:8080/messages", {
+    next: {
+      revalidate: 20,
+    },
+  });
   const messages = await response.json();
   const totalMessages = messages.length;
 
